cbc76d346f6efa19e1cf732ff76c8f30557ef1b2
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-03-14 23:27:47 -0500

    feat: :sparkles: Footer component done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer>
             <div class="container" id="subscribe-box">
                 <div class="col-md-6 ">
-                    <h1 class="col-md-12 pb-5">Subscribe and get exclusive deals & offer</h1>
+                    <h1 class="col-md-12 pb-5">Subscribe and get exclusive deals & offers</h1>
                     <div class="input-group position-relative">
                         <span class="input-group-text border border-0 bg-white ps-3"><img src="../img/email.png" alt="Email icon"/></span>
                         <input type="text" class="form-control form-control-lg pt-4 pb-4 ps-0 border-0 bg-white fs-6" placeholder="Enter your mail" aria-label="Example text with button addon" aria-describedby="button-addon1"/>
@@ -17,48 +17,74 @@ export default function Footer() {
                 <div class="background-image-bottom-left"></div>     
             </div>
             <div class="container">
-                <div class="d-flex flex-row">
-                    <div class="me-auto flex-fill">
-                        <h4 class="brand-footer">Trabook 
-                            <img src="../icons/Vector.png"/>
-                        </h4>
-                        <p class="text-secondary text-wrap lh-lg" style="width: 15rem;">Book your trip in minute, get full Control for much longer.</p>
-                        <div class="social-icons">
-                            <a href="#" class="inline-block"><img src="../icons/facebook.png"/></a>
-                            <a href="#" class="inline-block"><img src="../icons/instagram.png"/></a>
-                            <a href="#" class="inline-block"><img src="../icons/x.png"/></a>
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5">
+                    <div class="col mb-2">
+                        <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+                            <img src="../images/img/Logo.png" alt="Trabook logo" />
+                        </a>
+                        <p class="text-body-secondary">Book your trip in a minute, get full control for much longer.</p>
+                        <div class="social-media d-flex gap-2 pt-2 pb-1 justify-content-start">
+                            <a href="#" class="btn">
+                                <i class="bi bi-facebook"></i>
+                            </a>
+                            <a href="#" class="btn">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+                            <a href="#" class="btn">
+                                <i class="bi bi-twitter"></i>
+                            </a>
                         </div>
                     </div>
-                    <div class="p-2 flex-fill">
+                    <div class="col mb-2"></div>
+                    <div class="col mb-2">
                         <h5>Company</h5>
-                        <div class="d-flex flex-column mb-3">
-                            <a href="#" >About</a>
-                            <a href="#" >Careers</a>
-                            <a href="#" >Logistic</a>
-                            <a href="#" >Privacy & Policy</a>
-                        </div>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">About</a>
+                            </li>
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">Careers</a>
+                            </li>
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">Logistic</a>
+                            </li>
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">Privacy & Policy</a>
+                            </li>
+                        </ul>
                     </div>
-                    <div class="p-2 flex-fill">
+                    <div class="col mb-2">
                         <h5>Contact</h5>
-                        <div class="d-flex flex-column mb-3">
-                            <a href="#" >Help/FAQ</a>
-                            <a href="#" >Press</a>
-                            <a href="#" >Affilates</a>
-                        </div>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">Help/FAQ</a>
+                            </li>
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">Press</a>
+                            </li>
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">Affiliates</a>
+                            </li>
+                        </ul>
                     </div>
-                    <div class="p-2 flex-fill">
+                    <div class="col mb-2">
                         <h5>More</h5>
-                        <div class="d-flex flex-column mb-3">
-                            <a href="#" >Press Centre</a>
-                            <a href="#" >Our Blog</a>
-                            <a href="#" >Low fare tips</a>
-                        </div>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">Press Centre</a>
+                            </li>
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">Our Blog</a>
+                            </li>
+                            <li class="nav-item mb-2">
+                                <a href="#" class="nav-link p-0 text-body-secondary">Low fare tips</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <hr/>
-                <div class="d-flex justify-content-between">
-                    <p class="text-secondary">Copyright, Trabook 2022. All rights reserved.</p>
-                    <p class="text-secondary">Terms & Conditions</p>
+                <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                    <p>© Trabook 2022. All rights reserved.</p>
+                    <a href="#" class="nav-link px-2 text-body-secondary">Terms & Conditions</a>
                 </div>
             </div>
         </footer>
