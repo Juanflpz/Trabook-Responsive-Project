d56f00b858ddf0bd5bb113195c68c855e5dd84a7
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-03-21 23:12:18 -0500

    corrections

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-//import logo from './logo.svg';
 import './App.css';
-import Component from './Component';
 import Navbar from './Components/Navbar.jsx';
-import Carousel from './Components/Carousel.jsx';
-import navbarProps from './Props/NavbarProps.js';
+import NavbarProps from './Props/NavbarProps.js';
 import GetStarted from './Components/GetStarted.jsx';
 import GetStartedProps from './Props/GetStartedProps.js'
 import Needs from './Components/Needs.jsx';
 import NeedsProps from './Props/NeedsProps.js';
 import Footer from './Components/Footer.jsx';
-import footerProps from './Props/FooterProps.js';
+import FooterProps from './Props/FooterProps.js';
 import Deals from './Components/Deals.jsx';
-import dealsProps from './Props/DealsProps.js';
+import DealsProps from './Props/DealsProps.js';
 import Vacation from './Components/VacationPlan.jsx';
-import vacationProps from './Props/VacationPlanProps.js';
+import VacationProps from './Props/VacationPlanProps.js';
 import Reviews from './Components/Reviews.jsx';
-import reviewsProps from './Props/ReviewsProps.js';
+import ReviewsProps from './Props/ReviewsProps.js';
 import Blog from './Components/Blog.jsx';
-import blogProps from './Props/BlogProps.js';
+import BlogProps from './Props/BlogProps.js';
 import React, { useState } from 'react';
 
-
 function App() {
-  const cards = [
-    { id: 1, title: 'Card 1', content: 'Content of Card 1' },
-    { id: 2, title: 'Card 2', content: 'Content of Card 2' },
-    { id: 3, title: 'Card 3', content: 'Content of Card 3' },
-  ];
-
   /*const [showLoginForm, setShowLoginForm] = useState(false);
   const [showRegisterForm, setShowRegisterForm] = useState(false);
 
@@ -44,61 +34,31 @@ function App() {
   return (
     <div className='App'>
       <Navbar
-        image={navbarProps.image}
-        li1={navbarProps.li1}
-        li2={navbarProps.li2}
-        li3={navbarProps.li3}
-        li4={navbarProps.li4}
-        li5={navbarProps.li5}
-        a1={navbarProps.a1}
-        a2={navbarProps.a2}/>
+        image={NavbarProps.image}
+        li1={NavbarProps.li1}
+        li2={NavbarProps.li2}
+        li3={NavbarProps.li3}
+        li4={NavbarProps.li4}
+        li5={NavbarProps.li5}
+        a1={NavbarProps.a1}
+        a2={NavbarProps.a2}/>
       <GetStarted 
         image = {GetStartedProps.image1}/>
       <Needs cards = {NeedsProps}/>
       <Deals 
-        cards = {dealsProps}/>   
+        cards = {DealsProps}/>   
       <Vacation
-        cards = {vacationProps}/>
+        cards = {VacationProps}/>
       <Reviews 
-        cards = {reviewsProps}/> 
+        cards = {ReviewsProps}/> 
       <Blog 
-        cards = {blogProps}/>
+        cards = {BlogProps}/>
       <Footer
-        image1 = {footerProps.image1}
-        image2 = {footerProps.image2}/>
+        image1 = {FooterProps.image1}
+        image2 = {FooterProps.image2}/>
     </div>
   );
 }
 
-/**
- *  <h1>Dynamic Carousel</h1>
-      <Carousel cards={cards}/>
- */
-/*
-<h1>Dynamic Carousel</h1>
-      <Carousel cards={cards} />
-      */
-//<img src={logo} className="App-logo" alt="logo" />
-
-/*
-<div className="App">
-      <header className="App-header">
-        <Navbar></Navbar>
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Component/>
-      </header>
-    </div>
-*/
 export default App;
 
