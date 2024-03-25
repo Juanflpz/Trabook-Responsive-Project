d8dbd007b23c0b04d8970e8dc37216670923f8c5
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-03-24 21:27:15 -0500

    feat(1): :sparkles: Deals prototype done

--- src/Components/Deals.jsx
+++ src/Components/Deals.jsx
@@ -7,13 +7,13 @@ export default function Deals({ cards }) {
     for (let i = 0; i < cards.length; i += 4) {
         groupedCards.push(cards.slice(i, i + 4));
     }
-    
+
     return (
         <section id="vacation">
             <div className="container col-xxl-8 my-5">
                 <div className="row">
                     <h3 className="centered-title">Exclusive <span className="orange-color">deals & discounts</span></h3>
-                    <p className="prf text-muted">Discover our fantastic early booking discounts<br/>& start planning your journey.</p>
+                    <p className="prf text-muted">Discover our fantastic early booking discounts<br />& start planning your journey.</p>
                 </div>
                 <div className="container d-flex justify-content-center">
                     <div id="discounts-carousel" className="carousel carousel-dark slide d-none d-lg-block" data-bs-ride="carousel">
@@ -57,13 +57,13 @@ export default function Deals({ cards }) {
                         </div>
                         <div className="arrows d-flex justify-content-center">
                             <button className="btn slider-btn me-3 my-3 p-0 border-0" type="button" data-bs-target="#discounts-carousel" data-bs-slide="prev">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="svg-left-arrow bi bi-arrow-left-circle" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="svg-left-arrow bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
                                 </svg>
                             </button>
                             <button className="btn slider-btn ms-3 my-3 p-0 border-0" type="button" data-bs-target="#discounts-carousel" data-bs-slide="next">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="svg-right-arrow bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="svg-right-arrow bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
                                 </svg>
                             </button>
                         </div>
--- src/Components/Needs.jsx
+++ src/Components/Needs.jsx
@@ -23,7 +23,7 @@ export default function Needs({ cards }) {
             <div className="container cards">
                 <div className="row justify-content-center">
                     <div className='col-lg-11'>
-                        <div id="discounts-carousel" className="carousel carousel-dark slide d-none d-lg-block mx-auto" data-bs-ride="carousel">
+                        <div id="needs-carousel" className="carousel carousel-dark slide d-none d-lg-block mx-auto" data-bs-ride="carousel">
                             <div className="carousel-inner">
                                 {groupedCards.map((group, index) => (
                                     <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
@@ -31,7 +31,7 @@ export default function Needs({ cards }) {
                                             {group.map(card => (
                                                 <div key={card.id} className={`card discounts-carousel-card card-exclusive m-3 ${index === 0 ? 'card-1' : 'card-2'}`}>
                                                     <img src={require(`../images/icons/${card.image}`)} className="card-icon-top mt-4 ms-4" alt="Sign-up-icon" style={{ maxWidth: '100%', height: 'auto' }} />
-                                                    <div className="card-body mb-1 ms-1 me-4">
+                                                    <div className="card-body mb-1 ms-1">
                                                         <h4 className="card-title-needs font-weight-bold mt-4 ms-2">{card.title}</h4>
                                                         <p className="card-text-needs text-muted ms-2 mt-1" dangerouslySetInnerHTML={{ __html: card.description }}></p>
                                                     </div>
