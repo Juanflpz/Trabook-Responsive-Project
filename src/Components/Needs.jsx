9f5a906e471908c745cbaf45ac4f4c96a58b2c78
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-03-25 20:49:04 -0500

    style: :lipstick: needs content fixed

--- src/Components/Needs.jsx
+++ src/Components/Needs.jsx
@@ -27,7 +27,7 @@ export default function Needs({ cards }) {
                             <div className="carousel-inner">
                                 {groupedCards.map((group, index) => (
                                     <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-                                        <div className="card-wrapper container-sm d-flex">
+                                        <div className="card-wrapper container-sm d-flex card-needs">
                                             {group.map(card => (
                                                 <div key={card.id} className={`card discounts-carousel-card card-exclusive m-3 ${index === 0 ? 'card-1' : 'card-2'}`}>
                                                     <img src={require(`../images/icons/${card.image}`)} className="card-icon-top mt-4 ms-4" alt="Sign-up-icon" style={{ maxWidth: '100%', height: 'auto' }} />
