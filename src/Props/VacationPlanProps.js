e1422f5614f76da2320add95f22c0613cea4c073
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-03-26 22:29:36 -0500

    feat: :lipstick: login and register container fixed

--- src/Props/VacationPlanProps.js
+++ src/Props/VacationPlanProps.js
@@ -1,13 +1,13 @@
 const cards = [
-    {id: 1, image: 'Roma.png', title: 'Rome, Italy', price: '5,42k', duration: '10 days trip', rate: '4.8'}, 
-    {id: 2, image: 'London.png', title: 'London, UK', price: '2,83k', duration: '3 days trip', rate: '4.7'},
-    {id: 3, image: 'Osaka.png', title: 'Osaka, Japan', price: '1,53k', duration: '7 days trip', rate: '5'},
-    {id: 4, image: 'Estambul.png', title: 'Estambul, Turkey', price: '3,71k', duration: '6 days trip', rate: '4.5'},
-    {id: 5, image: 'Cabo.png', title: 'Cabo, Sudafrica', price: '4,5k', duration: '12 days trip', rate: '3.9'},
-    {id: 6, image: 'Hong Kong.png', title: 'Hong Kong, China', price: '1,92k', duration: '5 days trip', rate: '4.2'},
-    {id: 7, image: 'Bangkok.png', title: 'Bangkok, Tailand', price: '2,03k', duration: '8 days trip', rate: '4.9'},
-    {id: 8, image: 'Buenos Aires.png', title: 'Buenos Aires, Argentina', price: '3,33k', duration: '5 days trip', rate: '4.8'},
-    {id: 9, image: 'Moscow.png', title: 'Moscow, Russia', price: '0,87k', duration: '3 days trip', rate: '4.3'},
+    {id: 1, image: 'vacation01.png', title: 'Rome, Italy', price: '5,42k', duration: '10 days trip', rate: '4.8'}, 
+    {id: 2, image: 'vacation02.png', title: 'London, UK', price: '2,83k', duration: '3 days trip', rate: '4.7'},
+    {id: 3, image: 'vacation03.png', title: 'Osaka, Japan', price: '1,53k', duration: '7 days trip', rate: '5'},
+    {id: 4, image: 'vacation04.png', title: 'Estambul, Turkey', price: '3,71k', duration: '6 days trip', rate: '4.5'},
+    {id: 5, image: 'vacation05.png', title: 'Cabo, Sudafrica', price: '4,5k', duration: '12 days trip', rate: '3.9'},
+    {id: 6, image: 'vacation06.png', title: 'Hong Kong, China', price: '1,92k', duration: '5 days trip', rate: '4.2'},
+    {id: 7, image: 'vacation07.png', title: 'Bangkok, Tailand', price: '2,03k', duration: '8 days trip', rate: '4.9'},
+    {id: 8, image: 'vacation08.png', title: 'Buenos Aires, Argentina', price: '3,33k', duration: '5 days trip', rate: '4.8'},
+    {id: 9, image: 'vacation09.png', title: 'Moscow, Russia', price: '0,87k', duration: '3 days trip', rate: '4.3'},
 ];
 
 export default cards;
